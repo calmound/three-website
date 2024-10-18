0a007c1a04cc8198a2b1d1b518b01f3047627928
sanmu <zhusen@oschina.cn>
2024-10-18 21:37:37 +0800

    init

---
+++ next.config.js
@@ -0,0 +1,36 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  },
+  // 其他配置项
+  webpack(config, { dev }) {
+    if (dev) {
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // 去除 console.log
+              drop_debugger: true, // 去除 debugger
+            },
+            output: {
+              comments: false, // 去除注释
+              beautify: false, // 使代码压缩为单行
+            },
+          },
+          extractComments: false, // 不提取注释
+        })
+      );
+
+      config.optimization.minimize = true;
+      config.devtool = "hidden-source-map";
+    }
+    return config;
+  },
+});
---
+++ theme.config.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: "文档中心",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s",
+      additionalLinkTags: [{ rel: "icon", href: "/favicon.ico" }],
+    };
+  },
+  // project: {
+  //   link: "https://github.com/shuding/nextra-docs-template",
+  // },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "",
+  },
+  search: {
+    placeholder: "关键字搜索", // 修改此处的文本
+  },
+  toc: {
+    title: "目录", // 右侧顶部title
+  },
+  feedback: {
+    content: "", // 修改此处的文本
+  },
+  editLink: {
+    text: "", // 右侧编辑链接文本
+  },
+  darkMode: false,
+};
+
+export default config;
