37847c21b31ca70532854b4b52e92e936bfe1376
sanmu <245708505@qq.com>
2024-11-25 00:19:52 +0800

    feat: replace Google Analytics integration with gtag.js for improved tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
-import { GoogleAnalytics } from "nextjs-google-analytics";
-
 const App = ({ Component, pageProps }) => {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
+      {/* <GoogleAnalytics trackPageViews /> */}
+      {/* Google tag (gtag.js) */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-F6K2YKDCMV"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F6K2YKDCMV');
+          `,
+        }}
+      />
       test code
       <Component {...pageProps} />
     </>
